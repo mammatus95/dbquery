3c55087a0e085b3034ef7dfed253342998c8130e
Morten <mortenkretschmer@yahoo.de>
2022-02-28 14:07:06 +0100

    add visulation

--- test.py
+++ test.py
@@ -1,5 +1,17 @@
+"""
+Script to query basic information about a database.
+
+Change log:
+Author            Date
+M. Kretschmer  25.02.2022      First implementation
+M. Kretschmer  28.02.2022      First steps of visulation, and improvements of the info option
+
+"""
+
 import argparse
 import sqlite3
+import numpy as np
+import matplotlib.pyplot as plt
 
 parser = argparse.ArgumentParser(description="Query a database and print the content on the shell")
 parser.add_argument("name", type=str, help="Database name")
@@ -7,7 +19,8 @@ parser.add_argument("--info", help="Info database: Prints table names, column na
 parser.add_argument("--table", "-t", type=str, help="Table name")
 parser.add_argument("--order", "-o", type=str, help="Ordered by column")
 parser.add_argument("--select", "-s", type=str, help="Which column to print out")
-parser.add_argument("--visulation", "-v", type=str, help="Visualize one or meore columns of an existing table in the database.\n Example: type lightnings")
+parser.add_argument("--number", "-n", type=str, help="Number of rows you want to print on terminal")
+parser.add_argument("--visulation", "-v", type=str, help="Visualize one or meore columns of an existing table in the database.\n Example: type,lightnings")
 
 #parser.add_argument("--where", "-w", type=str, help="Filter database")
 #parser.add_argument("--whereval", "-ov", type=str, help="Filter by value?")
@@ -18,6 +31,11 @@ args = parser.parse_args()
 con = sqlite3.connect(args.name)
 cur = con.cursor()
 
+if args.number:
+    printrows_num=int(args.number)
+else:
+    printrows_num=5
+
 if args.info:
     info_query=[]
     for row in cur.execute('SELECT sql FROM sqlite_schema ORDER BY tbl_name, type DESC, name'):
@@ -25,7 +43,9 @@ if args.info:
         info_query.append(row[0].split("\n"))
     for info_str in info_query:
         tablenames = info_str[0].split(" ")[2]
-        print("Table name: " + tablenames + "\n\nContains Columns:")
+        print("Table name: " + tablenames)
+        rows_num = cur.execute("SELECT COUNT(*) FROM " + tablenames).fetchall()[0][0]
+        print("number of rows: " + str(rows_num) + "\n\nContains Columns:")
         info_str[1] = info_str[1].replace("               (","")
         col = info_str[1].split(", ")
         print("Column name      ! datatyp\n--------------------------")
@@ -39,14 +59,14 @@ if args.info:
         print(head_str)
         exe_str='SELECT * FROM ' + tablenames
         count=0
-        rows_example=5
+
         for row in cur.execute(exe_str):
             row_content=""
             count+=1
             for i in row:
                 row_content += '{0: <18} ! '.format(i)
             print(row_content)
-            if count > rows_example:
+            if count > printrows_num:
                 break
         print("\n\n")
     exit(0)
@@ -68,9 +88,36 @@ if args.order:
     exe_str+= " ORDER BY " + args.order
 
 
+
+#terminal output!
 print(" Command:", exe_str)
+count=0
 for row in cur.execute(exe_str):
+    count+=1
     print(row)
-print(" Command:", exe_str)
+    if count > printrows_num:
+        break
 
-
+#visulation
+if args.visulation:
+    visulation = args.visulation.replace(" ", ",")
+    col_num = len(visulation.split(","))
+    rows_num = rows_num = cur.execute("SELECT COUNT(*) FROM " + tablenames).fetchall()[0][0]
+    content_array = np.zeros((col_num,rows_num))
+    #print(content_array.shape)
+    exe_str='SELECT ' + visulation + ' FROM ' + tablenames
+    i=0
+    for row in cur.execute(exe_str):
+        content_array[0,i] = row[0]
+        content_array[1,i] = row[1]
+        i+=1
+    #print(content_array[1,2])
+    num_bins = 50
+    for j in range(0,col_num):
+        fig, ax = plt.subplots()
+        ax.set_title(tablenames + " by " + visulation.split(",")[j])
+        n, bins, patches = ax.hist(content_array[j,:], num_bins, density=True)
+        ax.set_xlabel(visulation.split(",")[j])
+        ax.set_ylabel('Probability density')
+        plt.savefig(visulation.split(",")[j] + ".png")
+        plt.close()
